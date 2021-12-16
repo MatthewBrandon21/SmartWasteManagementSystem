5e19bb0110ceb37039505ea89d31810d10575bbb
rellpa <michaeliandiraa@gmail.com>
2021-12-16 11:37:39 +0800

    lastt

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: "http://localhost:5000" })
+const API = axios.create({ baseURL: "https://smartwastemanagement-api.herokuapp.com" })
 // const url = "https://smartwastemanagement-api.herokuapp.com"
 
 // login
